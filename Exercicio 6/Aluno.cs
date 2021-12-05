64a95d28a3e483ffd31b21e4a654c1435c73e2b1
Quiel <Quiel@DESKTOP-QPM4FGN>
2021-12-05 07:46:31 -0300

    Add project files.

---
+++ Exercicio 6/Aluno.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio_6
+{
+    internal class Aluno
+    {
+        public string Nome { get; set; }
+        public double PrimeiraNota { get; set; }
+        public double SegundaNota { get; set; }
+        public double TerceiraNota { get; set; }
+
+        public Aluno(string nome, double primeiraNota, double segundaNota, double terceiraNota)
+        {
+            Nome = nome;
+            PrimeiraNota = primeiraNota;
+            SegundaNota = segundaNota;
+            TerceiraNota = terceiraNota;
+        }
+
+        public double CalcularNota()
+        {
+            return (PrimeiraNota + SegundaNota + TerceiraNota) / 3;
+        }
+
+        public double VerificarQuantosFaltam()
+        {
+            return 100 - CalcularNota();
+        }
+    }
+}
---
+++ Exercicio 6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercicio_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Nome do Aluno:");
+                string nome = Console.ReadLine();
+                //pontuação de notas de 0 a 100
+                Console.WriteLine("Digite as três notas do aluno:");
+                double primeiraNota = double.Parse(Console.ReadLine());
+                double segundaNota = double.Parse(Console.ReadLine());
+                double terceiraNota = double.Parse(Console.ReadLine());
+
+                Aluno aluno = new Aluno(nome, primeiraNota, segundaNota, terceiraNota);
+
+                Console.WriteLine($"\n{aluno.Nome}: NOTA FINAL = {aluno.CalcularNota().ToString("F2")}");
+
+                if (aluno.CalcularNota() < 60)
+                {
+                    Console.WriteLine($"REPROVADO\nFALTAM {aluno.VerificarQuantosFaltam().ToString("F2")} PONTOS");
+                }
+                else
+                {
+                    Console.WriteLine($"APROVADO");
+                }
+
+                Console.WriteLine("\nPressione qualquer tecla para finalizar");
+                Console.ReadKey();
+            }
+            catch (Exception falha)
+            {
+                Console.WriteLine($"> {falha.Message}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
